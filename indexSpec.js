61b4aca3d8d419d8eb097a125a86434be0896e6e
Pristine Tran <pristinetran@tycoint.com>
2017-03-01 17:04:09 -0800

    first commit

---
+++ indexSpec.js
@@ -0,0 +1,9 @@
+var chai = require('chai')
+var expect = require('chai').expect;
+
+describe('Sanitize', function () {
+	it('returns lowercase of a string', function(){
+		expect('HELLO WORLD').to.equal('HELLO WORLD')
+	})
+	it('remove any hyphen')
+})
---
+++ readme.md
@@ -0,0 +1,4 @@
+
+Src: http://build-podcast.com/mocha
+
+https://www.youtube.com/watch?v=Q8Jl85FJz4E&list=PLw5h0DiJ-9PAdZwGJCYb7a9P2mJHayQQ3
